c9af72372588932189a666b449664d3540e0285d
dsosuliev <dsosuliev@ascendix.net>
2020-12-22 21:24:14 +0200

    Initial commit

---
+++ src/main/java/com/sonar/cloud/test/demo/ScdpApplication.java
@@ -0,0 +1,13 @@
+package com.sonar.cloud.test.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScdpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScdpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonar/cloud/test/demo/ScdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonar.cloud.test.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScdpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
